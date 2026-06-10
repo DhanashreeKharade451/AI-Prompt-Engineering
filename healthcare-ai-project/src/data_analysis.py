cfdb2b282426358799d9bef6bb7de205091bdeac
Dhanashree <dhanashreeakharade@gmail.com>
2026-06-09 22:27:27 -0400

    created project structure

---
+++ healthcare-ai-project/src/data_analysis.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# Load dataset
+df = pd.read_csv("../data/healthcare-ai-project/data/Test_Inpatientdata-1542969243754.csv")
+
+# Display first 5 rows
+print(df.head())
+
+# Dataset information
+print(df.info())
+
+# Statistical summary
+print(df.describe())
